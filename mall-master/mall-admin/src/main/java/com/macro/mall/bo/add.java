d345ae9d318ff18a802aa43307cd183500866e10
craywen <1614241322@qq.com>
2020-01-10 20:22:45 +0800

    aa

---
+++ mall-master/mall-admin/src/main/java/com/macro/mall/bo/add.java
@@ -0,0 +1,12 @@
+package com.macro.mall.bo;
+
+/**
+ * Created by craywen on 2020/1/10 20:09
+ *
+ * @description
+ */
+public class add {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
